b67391cd98da36ebd98b73720b003779b058889c
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-01 13:05:17 +0200

    Added string ex file

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Asmir";
+    var myLastName = "Sulejmanovic";
+    var myHomeTown = "Vogosca";
+    // only change code above this line
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+
+module.exports = main;
