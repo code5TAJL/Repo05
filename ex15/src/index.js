a1219532a1a3e50a57cf6a927ca570cd6a939b46
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-02 08:55:10 +0200

    Added last JS ex for today

---
+++ ex15/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this code
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    
+    var wordBlanks = myAdjective  + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+    // Only change code above this code
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
